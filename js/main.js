07d542c5edad2eb311a3b0f763d003580e18341c
TahmidSajid <tahmidkhansajid@gmail.com>
2023-02-09 01:59:26 +0600

    done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.testi_slider').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    Arrow: true,
+    nextArrow: '.testi_right',
+    prevArrow: '.testi_left',
+});
+
+
+
+var mixer = mixitup('.port_page_main');
